97349457f3f9761ee717578a597f0757ba1e2f4a
Hubert Kwiecień <hubistef16@wp.pl>
2021-10-09 19:15:41 +0200

    added new functionality to Vec2D.h

--- Vec2D.h
+++ Vec2D.h
@@ -2,4 +2,18 @@ struct Vec2D
 {
     int x;
     int y;
+
+    vec2D(): x(0),y(0){}
+    vec2D(int x, int y): x(x),y(y){}
+    vec2D(const Vec2D& v): x(v.x), y(v.y){}
+
+    Vec2D& operator += (vec2D&v)
+    {
+        return vec2D(x + v.x, y + v.y);
+    }
+
+     Vec2D& operator -= (vec2D&v)
+    {
+        return vec2D(x - v.x, y - v.y);
+    }
 };
